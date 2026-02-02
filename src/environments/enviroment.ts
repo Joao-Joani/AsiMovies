377d97fe4a668f01c4ae63ea3f9d37a2cac71f05
Joao Joani <jgabriel.joani@gmail.com>
2026-02-02 00:26:23 -0300

    Add login component and integrate AngularFire.

---
+++ src/environments/enviroment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production : false,
+    firebase : {
+        apiKey: "AIzaSyBnvyagOCUHZlh6idX4Onjp7zuQf9ONDK0",
+        authDomain: "asimovies-d5e3e.firebaseapp.com",
+        projectId: "asimovies-d5e3e",
+        storageBucket: "asimovies-d5e3e.firebasestorage.app",
+        messagingSenderId: "127591187464",
+        appId: "1:127591187464:web:9862297600b36f7ce1fdbd",
+        measurementId: "G-HK9S3292XS"
+    }
+};
